6bf78b9abdf0962bf4981910cb21a891e2662713
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-03-27 10:58:11 +0200

    add file for viterbi path

---
+++ HMM/get_viterbi.m
@@ -0,0 +1,12 @@
+% Inputs:
+%   f_hmm - file name of the HMM model, e.g. hmm_41
+
+function get_viterbi(f_hmm)
+    DIROUT = 'outputs/'; % change if needed
+    load X
+    load T
+    load(sprintf('%d/%d',DIROUT,f_hmm))
+
+    [viterbipath] = hmmdecode(X,T,hmm,1); % get viterbi path
+    save(sprintf('%d/viterbipath',DIROUT),'viterbipath')
+end
