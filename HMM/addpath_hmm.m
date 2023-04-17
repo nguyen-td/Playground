5cd0431c877c18682802bfe65c0e8f5af90428fa
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-04-17 11:56:34 +0200

    modify matlab path

--- HMM/addpath_hmm.m
+++ HMM/addpath_hmm.m
@@ -1,3 +1,5 @@
+% add matlab paths on Ramen
 function addpath_hmm
-    addpath(genpath('/home/tdnguyen/git_repos/HMM-MAR')) % on Ramen
+    addpath(genpath('/home/tdnguyen/git_repos/HMM-MAR')) 
+    addpath(genpath('/home/tdnguyen/git_repos/Playground/HMM')) 
 end
