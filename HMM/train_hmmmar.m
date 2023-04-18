b65453d2dae94990d0bf11b202646e95966fd299
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-04-18 10:27:23 +0200

    bugfix

--- HMM/train_hmmmar.m
+++ HMM/train_hmmmar.m
@@ -62,12 +62,12 @@ function train_hmmmar(k, order, covtype, viterbi, data_mod)
     fprintf(felapsed_time,'Elapsed time is %d seconds.',t_end);
     fclose(felapsed_time);
     
-    hmm_name = sprintf(strcat(DIROUT,'hmm_%d%d_%d_',covtype,'.mat'), k, order, data_mod); 
-    gamma_name = sprintf(strcat(DIROUT,'gamma_%d%d_%d_',covtype,'.mat'), k, order, data_mod); 
+    hmm_name = sprintf(strcat(DIROUT,'hmm_%d%d_%',covtype,'_',data_mod,'.mat'), k, order); 
+    gamma_name = sprintf(strcat(DIROUT,'gamma_%d%d_',covtype,'_',data_mod,'.mat'), k, order); 
     save(hmm_name, 'hmm', '-v7.3') % saving variables > 2GB
     save(gamma_name, 'Gamma')
     if viterbi
-        vpath_name = sprintf(strcat(DIROUT,'vpath_%d%d_%d_',covtype,'.mat'), k, order, data_mod); 
+        vpath_name = sprintf(strcat(DIROUT,'vpath_%d%d_',covtype,'_',data_mod,'.mat'), k, order); 
         save(vpath_name, 'vpath') 
     end
     if strcmpi(data_mod,'fmri')
