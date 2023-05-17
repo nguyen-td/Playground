9bf855f18c055abd1a7b7c96aac43b65434e8b22
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-05-17 11:50:43 +0200

    bugfix

--- HMM/train_hmmmar.m
+++ HMM/train_hmmmar.m
@@ -18,8 +18,12 @@ function train_hmmmar(k, order, covtype, viterbi, data_mod, load_data)
     
     % load data or generate simulated data
     if load_data
-        load(sprintf(strcat(DIROUT,'gen_%d%d_', data_mod, '.mat'), k, order));
-        X = data;
+        try
+            load(sprintf(strcat(DIROUT,'gen_%d%d_', data_mod, '.mat'), k, order));
+            X = out_genar{1};
+        catch
+            error('File not present. Please check if your gen_*.mat file matches the number of states and AR model order.')
+        end
     else
         if strcmpi(data_mod, 'fmri')
             [X, T] = gen_fmri;
