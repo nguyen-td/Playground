4d4a50de5f870101f38ea7dc596707aab653a724
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-05-17 11:47:34 +0200

    remove try-catch

--- HMM/train_hmmmar.m
+++ HMM/train_hmmmar.m
@@ -18,12 +18,8 @@ function train_hmmmar(k, order, covtype, viterbi, data_mod, load_data)
     
     % load data or generate simulated data
     if load_data
-        try
-            load(sprintf(strcat(DIROUT,'gen_%d%d_', data_mod, '.mat'), k, order));
-            X = data;
-        catch
-            error('File not present. Please check if your gen_*.mat file matches the number of states and AR model order.')
-        end
+        load(sprintf(strcat(DIROUT,'gen_%d%d_', data_mod, '.mat'), k, order));
+        X = data;
     else
         if strcmpi(data_mod, 'fmri')
             [X, T] = gen_fmri;
